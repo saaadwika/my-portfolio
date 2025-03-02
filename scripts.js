5341aa84b94c2a12866226cde16624869460eccb
saaadwika <147240817+saaadwika@users.noreply.github.com>
2025-03-02 15:53:21 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message!');
+    // Here, you can add code to send the form data to your email or server
+});
